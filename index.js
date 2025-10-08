5ebcadfff120e583311ba26d0bfe7ed9165dd9db
ibheehe <ibrahimasif25@gmail.com>
2025-10-08 12:22:42 +0100

    initial veresion

---
+++ index.js
@@ -0,0 +1,22 @@
+var http = require("http"); 
+const port = 8000; 
+
+http.createServer(function(req, res) { 
+     res.writeHead(200, { "Content-Type": "text/html" });
+res.end(`<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>About Muhammad Ibrahim Asif</title> //my name
+</head>
+<body>
+    <h1 style="color: red;">Muhammad Ibrahim Asif</h1>
+    <h2>Computer Science Student year 3</h2>
+    <p>I Love playing soulsborne games and souls like games. I have a pet cat named Chairman Meow.</p>
+    <p>This is the first labwork of the class. and i think i did pretty good.</p>
+</body>
+</html>`);
+
+    }).listen(port, function() { 
+        console.log(`Node server is running on port ${port}...`); 
+}); 
